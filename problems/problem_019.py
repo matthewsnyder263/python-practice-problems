0b1cf95babfb78890dcdd333791f1ff3fffa5079
Matt Snyder <msnyd87@gmail.com>
2023-03-24 15:57:44 -0400

    Answer to 19,20 problem

--- problems/problem_019.py
+++ problems/problem_019.py
@@ -15,4 +15,15 @@
 #   * y is less than or equal to rect_y + rect_height
 
 def is_inside_bounds(x, y, rect_x, rect_y, rect_width, rect_height):
-    pass
+    if x >= rect_x and x <= (rect_x + rect_width) and y >= rect_y and y <= (rect_y + rect_height):
+        return True
+    else:
+        return False
+x = 20
+y = 20
+rect_x = 3
+rect_y = 3
+rect_width = 3
+rect_height = 3
+
+print(is_inside_bounds(x, y, rect_x, rect_y, rect_width, rect_height))
--- problems/problem_020.py
+++ problems/problem_020.py
@@ -3,4 +3,19 @@
 # to 50% of the number of people in the members list.
 
 def has_quorum(attendees_list, members_list):
-    pass
+    if attendees_list >= (members_list / 2):
+        return True
+    else:
+        return False
+
+attendees_list = 49
+members_list = 100
+
+print(has_quorum(attendees_list, members_list))
+
+
+#set variable for total in attendence
+#    if total_attendees =
+#set variable to 50% of members_list
+#see if var1 >= var2
+#return true else false
