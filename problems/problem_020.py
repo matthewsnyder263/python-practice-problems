a5bf9498425aa8696e32acf15903cde420148d81
Matt Snyder <msnyd87@gmail.com>
2023-03-24 16:06:52 -0400

    answer to problem_20

--- problems/problem_020.py
+++ problems/problem_020.py
@@ -2,20 +2,31 @@
 # of the people in the attendees list is greater than or equal
 # to 50% of the number of people in the members list.
 
+
+# ## MY ANSWER DOESN'T TAKE INTO ACCOUNT THE LENGHT
+#def has_quorum(attendees_list, members_list):
+#    if attendees_list >= (members_list / 2):
+#        return True
+#    else:
+#        return False
+
+# attendees_list = 49
+# members_list = 100
+
+# print(has_quorum(attendees_list, members_list))
+
 def has_quorum(attendees_list, members_list):
-    if attendees_list >= (members_list / 2):
+    num_attendees = len(attendees_list)
+    num_members = len(members_list)
+    if num_attendees >= num_members / 2:
         return True
     else:
         return False
 
-attendees_list = 49
-members_list = 100
+attendees_list = ["mike", 1, 2, 3, 4, 5]
+members_list = ["jon", 1, 2,3,4,5,6,3,3,3,3,3]
 
+print (len(members_list))
+print (len(attendees_list))
 print(has_quorum(attendees_list, members_list))
 
-
-#set variable for total in attendence
-#    if total_attendees =
-#set variable to 50% of members_list
-#see if var1 >= var2
-#return true else false
